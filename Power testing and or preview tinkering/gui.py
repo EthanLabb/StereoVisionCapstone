5b6415567d265e1cb0acdfcaac76c052dafed154
uaswong6 <118694685+uaswong6@users.noreply.github.com>
2026-01-07 17:32:04 -0700

    Update gui.py
    need to improve more but technically works

--- Power testing and or preview tinkering/gui.py
+++ Power testing and or preview tinkering/gui.py
@@ -1,21 +1,60 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("My App")
+        buttonheight=120
 
-        button = QPushButton("Press Me!")
-        button.setCheckable(True)
-        button.clicked.connect(self.the_button_was_clicked)
+        self.setWindowTitle("Stereo Vision Capstone")
 
-        # Set the central widget of the Window.
-        self.setCentralWidget(button)
+        layout=QVBoxLayout()
+        layout.addWidget(QPushButton("1"))
+        layout.addWidget(QPushButton("2"))
+        layout.addWidget(QPushButton("3"))
+        layout.addWidget(QPushButton("4"))
 
-    def the_button_was_clicked(self):
-        print("Clicked!")
+
+        button1 = QPushButton("1")
+        button1.clicked.connect(self.button1click)
+        button1.setFixedHeight(buttonheight)
+
+        button2 = QPushButton("2")
+        button2.clicked.connect(self.button2click)
+        button2.setFixedHeight(buttonheight)
+
+        button3 = QPushButton("3")
+        button3.clicked.connect(self.button3click)
+        button3.setFixedHeight(buttonheight)
+
+        button4 = QPushButton("4")
+        button4.clicked.connect(self.button4click)
+        button4.setFixedHeight(buttonheight)
+
+        
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+    def button1click(self):
+        #first button code
+        print("hi1")
+
+    def button2click(self):
+        #second button code
+        print("hi2")
+
+    def button3click(self):
+        #first button code
+        print("hi3")
+
+    def button4click(self):
+        #first button code
+        print("hi4")
+
+
+    
 
 app = QApplication(sys.argv)
 
